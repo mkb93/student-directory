c231d77f5d3a075c57887120fe6dc9c550d0c358
marco <nfk18@hotmail.co.uk>
2023-03-22 14:42:18 +0000

    adding @student refactor

--- interactive_menu.rb
+++ interactive_menu.rb
@@ -2,12 +2,12 @@
 @center_amount ='please enter the name of the students and the cohort they are in'
 .length
 @cohorts = [:January, :April, :July, :November]
+#make an empty array
 #methods
 def input_students 
   puts 'please enter the name of the students and the cohort they are in'.center(@center_amount)
   puts "To finish, do not enter a name and press enter".center(@center_amount)
-  #make an empty array
-  students = []
+  
   #get student name
   name = gets[0...-1]
   
@@ -29,13 +29,11 @@ def input_students
       end
     end
     # add student to the array
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students".center(@center_amount)
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} students".center(@center_amount)
     #get another name and cohort from user
     name = gets[0...-1]
   end
-  #return students array 
-  students
 end
 
 def print_header
@@ -54,31 +52,39 @@ def print(names)
   end
 end
 
-def print_footer(names)
-  if names.count >1
-    puts "Overall, we have #{names.count} great students".center(@center_amount)
-  elsif names.count == 1 
-    puts "Overall, we have #{names.count} great student".center(@center_amount)
+def print_footer
+  if @students.count >1
+    puts "Overall, we have #{@students.count} great students".center(@center_amount)
+  elsif @students.count == 1 
+    puts "Overall, we have #{@students.count} great student".center(@center_amount)
   end
 end
 
+def print_menu
+  # print the interactive menu method
+  puts 'press 1 for input the students'.center(@center_amount)
+  puts 'press 2 for show the students'.center(@center_amount)
+  puts 'press 9 for exit'.center(@center_amount)
+end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer
+end
 def interactive_menu
-  students = []
+  @students = []
   loop do
-    # print the interactive menu
-    puts 'press 1 for input the students'.center(@center_amount)
-    puts 'press 2 for show the students'.center(@center_amount)
-    puts 'press 9 for exit'.center(@center_amount)
+    #print menu
+    print_menu
     # take the useer input and save it
     selection = gets.chomp
     #do what the user asked
     case selection
       when "1"
-        students = input_students
+        input_students
       when "2"
-        print_header
-        print(students)
-        print_footer(students)
+    show_students
       when "9"
         exit
       else
